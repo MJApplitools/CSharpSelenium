c5b5c42f61012465b6729a5034b0ca49f8cc251a
Matt Jasaitis <mattjasaitis@Matts-MacBook-Pro.local>
2019-06-20 09:23:11 -0700

    setup

---
+++ CSharpTutorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Applitools.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace CSharpTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            var eyes = new Eyes();
+            eyes.ForceFullPageScreenshot = true;
+            eyes.ApiKey = Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY");
+            try
+            {
+                //Dictionary<string, string> testinfo = getTestInfoForPart(args); 
+                eyes.Open(driver, "C# Demo", "Login Window", new Size(600, 800));
+                driver.Url = "https://demo.applitools.com";
+                eyes.CheckWindow("Login window test");
+                eyes.Close();
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                driver.Quit();
+                eyes.AbortIfNotClosed(); 
+            }
+        }
+    }
+}
